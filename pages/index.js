1154666ebc36ff796cd5c1d1c001de892899169f
Franciscodev <francysco.cesg@gmail.com>
2024-10-12 19:23:41 +0000

    Primeiros passos

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Hello World, estou aprendendo!
+        Muito massa cara, estou empolgado demais, espero aprender de maneira construtiva de verdade!
+    </h1>
+}
+export default Home;
